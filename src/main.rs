d026f07f59be7afed54f9d81ca5d837ed7c5f4ca
Jonas Kalderstam <jonas@kalderstam.se>
2017-03-13 01:06:32 +0100

    Cleaning up arguments

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,46 @@
 extern crate clap;
 extern crate html2runes;
 
-use clap::App;
+use clap::{App, Arg};
 
 use html2runes::parse::convert_stdin;
+
+use std::str::FromStr;
+
+enum Format {
+    Markdown,
+}
+
+impl FromStr for Format {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Format, ()> {
+        match s {
+            "markdown" => Ok(Format::Markdown),
+            _ => Err(()),
+        }
+    }
+}
 
 fn main() {
 
     let args = App::new("html2textrs")
         .version("0.1")
-        .about("Converts html to plain text")
-        //.arg(Arg::with_name("version")
-        //    .short("v")
-        //    .long("version")
-        //    .help("display the program's version and exit"))
+        .about("Converts html from STDIN to plain text on STDOUT.")
+        .arg(Arg::with_name("format")
+            .short("f")
+            .long("format")
+            .help("Plain text format to use")
+            .possible_values(&["markdown"])
+            .default_value("markdown"))
         .get_matches();
 
-    let result = convert_stdin();
+    // Default value exists
+    let format = args.value_of("format").unwrap();
+    let format = format.parse::<Format>().unwrap();
+
+    let result = match format {
+        Format::Markdown => convert_stdin(),
+    };
     println!("{}", result)
 }
